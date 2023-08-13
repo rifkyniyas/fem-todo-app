e57efb342366b6cbaee7389d31bafedfe36ef472
Rifky Niyas <63934498+MohammadRifky@users.noreply.github.com>
2023-08-13 17:38:45 +0530

    completed adding data

--- src/redux/TodoSlice.js
+++ src/redux/TodoSlice.js
@@ -1,22 +1,26 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { v4 as uuidv4 } from "uuid";
 
-const initialState = [
-  {
-    id: uuidv4(),
-    text: "Complete online JavaScript course",
-    isCompleted: true,
-  },
-  { id: uuidv4(), text: "Jog around the park 3x", isCompleted: false },
-  { id: uuidv4(), text: "10 minutes meditation", isCompleted: false },
-  { id: uuidv4(), text: "Read for 1 hour", isCompleted: false },
-  { id: uuidv4(), text: "Pick up groceries", isCompleted: false },
-  {
-    id: uuidv4(),
-    text: "Complete Todo App on Frontend Mentor",
-    isCompleted: false,
-  },
-];
+const initialState = {
+  todos: [
+    {
+      id: uuidv4(),
+      text: "Complete online JavaScript course",
+      isCompleted: true,
+    },
+    { id: uuidv4(), text: "Jog around the park 3x", isCompleted: false },
+    { id: uuidv4(), text: "10 minutes meditation", isCompleted: false },
+    { id: uuidv4(), text: "Read for 1 hour", isCompleted: false },
+    { id: uuidv4(), text: "Pick up groceries", isCompleted: false },
+    {
+      id: uuidv4(),
+      text: "Complete Todo App on Frontend Mentor",
+      isCompleted: false,
+    },
+  ],
+
+  filter: "all",
+};
 
 export const todoSlice = createSlice({
   name: "todos",
@@ -26,13 +30,17 @@ export const todoSlice = createSlice({
       state.push(action.payload);
     },
     toggleCompletion: (state, action) => {
-      const index = state.findIndex((todo) => todo.id === action.payload);
-      if (index !== -1) state[index].isCompleted = !state[index].isCompleted;
+      const todo = state.find((todo) => todo.id === action.payload);
+      if (todo) todo.isCompleted = !todo.isCompleted;
     },
     deleteTodo: (state, action) => {
-      return state.filter((todo) => todo.id !== action.payload);
+      const index = state.findIndex((todo) => todo.id === action.payload);
+      if (index !== -1) state.splice(index, 1);
     },
     clearTodos: () => [],
+    setFilter: (state, action) => {
+      state.filter = action.payload;
+    },
   },
 });
 
